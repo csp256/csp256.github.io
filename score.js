3287f4c7cec9b8fecb53c2b23f5491736628b5d2
Christopher Parker <csp256@users.noreply.github.com>
2021-09-16 22:28:41 -0700

    Update score.js

--- score.js
+++ score.js
@@ -1,5 +1,4 @@
-function round(x, decimal_places)
-{
+function round(x, decimal_places) {
   if (decimal_places === undefined) {
     decimal_places = 2;
   }
@@ -15,13 +14,11 @@ function calculate() {
   var closing = Number(document.getElementsByName("closing")[0].value);
   var rate = Number(document.getElementsByName("rate")[0].value);
 
-
   var numerator = rent - expense
   var denominator = price + closing
   var offset = rate;
 
   var score = round( numerator / denominator - offset, 1 );
-  console.log(score);
 
   var out_element = document.getElementsByName('output')[0];
   out_element.innerHTML = "Score = <b><big>" + String(score) + "</big></b>";
